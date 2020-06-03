844cd68b87c13664ad370f5e9a4099cbca989ea6
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-03 05:39:29 +0100

    Add inspec tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ controls/autoconf_works.rb
@@ -0,0 +1,25 @@
+title 'Tests to confirm autoconf works as expected'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'autoconf')
+
+control 'core-plans-autoconf-works' do
+  impact 1.0
+  title 'Ensure autoconf works as expected'
+  desc '
+  '
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+  
+  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} autoconf --version") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /autoconf \(GNU Autoconf\) #{plan_pkg_version}/ }
+    its('stderr') { should be_empty }
+  end
+end
